55038dbbba8f888db27b45da4492ba4f59434922
Koteswara1996 <koteswararaomukkapati1996@gmail.com>
2026-09-26 17:24:51 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
      non-GET         untouched
 */
 
-const CACHE_VERSION = 'btw-v55';
+const CACHE_VERSION = 'btw-v56';
 const SHELL_CACHE = CACHE_VERSION + '-shell';
 const RUNTIME_CACHE = CACHE_VERSION + '-runtime';
 const RUNTIME_LIMIT = 60;
